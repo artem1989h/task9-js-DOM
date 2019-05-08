8afa51b310caafd7f4b935c8dd7f3fcc7a5d6a01
Artem Habinskyi <a.habinskyi@gmail.com>
2019-05-08 15:06:04 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,36 +1,65 @@
 const display = document.querySelector('.display');
 const digits = document.querySelectorAll('.digits button');
-digits.forEach( btn => btn.addEventListener('click', digitPressed) );
+const opers = document.querySelectorAll('.opers button');
+
+digits.forEach(btn => btn.addEventListener('click', digitPressed));
+opers.forEach(btn => btn.addEventListener('click', operPressed));
+
 function digitPressed(e) {
     e.preventDefault();
     display.value += e.target.innerText;
-}
-const opers = document.querySelectorAll('.opers button');
-opers.forEach( btn => btn.addEventListener('click', operPressed) );
+    }
+
 function operPressed(e) {
     e.preventDefault();
     display.value += e.target.innerText;
-}
+    }
+    
+    
+// function isOperatorChar(str) {
+//     return (str == '+' || str == '-' || str == '*' || str == '/')
+// }
+    
+// function operPressed(e) {
+//     e.preventDefault();
+//     let lastChar = display.value.length - 1;
+//     if (isOperatorChar(display.value.charAt(lastChar))) {
+//         display.value = display.value;
+//     }
+//     else {
+//         display.value += e.target.innerText;
+//     }
+// }
 
-document.querySelector('.equal',).addEventListener('click', equalPressed);
+document.querySelector('.equal', ).addEventListener('click', equalPressed);
+
 function equalPressed(e) {
     e.preventDefault();
     display.value = eval(display.value);
-} 
+    }
 
-document.querySelector('.split',).addEventListener('click', splitPressed);
-function splitPressed(n) {
-  n = +n;  // Coerce to number.
-  if (!n) {  // Matches +0, -0, NaN
-  display.value = Error('Invalid dividend ' + n);
-  }
+const clearButton = document.createElement('clearButton');
+clearButton.textContent = 'C';
+clearButton.addEventListener('click', clearOnClick);
 
+function clearOnClick(e) {
+    e.preventDefault();
+    display.value = '0';
+    }
 
-// document.querySelector('.split').addEventListener('click', splitPressed);
-// function splitPressed(n) {
-//   n = +n;  // Coerce to number.
-//   if (!n) {  // Matches +0, -0, NaN
-//     throw new Error('Invalid dividend ' + n);
-//   }
-//   return n;
-// }
+function equalPressed(e) {
+        e.preventDefault();
+        if (display.value === 'error') {
+            return;
+        }
+        const parsedValue = parseFloat(eval(display.value));
+        if (!display.value) {
+            display.value = 0;
+        }
+        else if (!Number.isFinite(parsedValue) || Number.isNaN(parsedValue)) {
+            display.value = 'error';
+        }
+        else {
+            display.value = eval(display.value);
+        }
+    }
